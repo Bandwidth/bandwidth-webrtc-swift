96550b16ec9ae916a9b1c3b630dc4988b098d43e
Mike Hamer <mhamer@bandwidth.com>
2020-12-29 11:15:08 -0500

    Include WebRTC framework

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Bandwidth/json-rpc-websockets.git", .branch("main"))
+        .package(url: "https://github.com/Bandwidth/json-rpc-websockets.git", .branch("main")),
+        .package(url: "https://github.com/alexpiezo/WebRTC.git", .upToNextMajor(from: "1.1.31567"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
