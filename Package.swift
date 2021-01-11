ff64ede609a666bf817fcbc1618179db5c0ec115
Mike Hamer <mhamer@bandwidth.com>
2021-01-11 12:26:26 -0500

    Rename BandwidthWebRTC to BandwidthRTC

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "BandwidthWebRTC",
-            targets: ["BandwidthWebRTC"]),
+            name: "BandwidthRTC",
+            targets: ["BandwidthRTC"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,11 +22,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "BandwidthWebRTC",
+            name: "BandwidthRTC",
             dependencies: [.product(name: "JSONRPCWebSockets", package: "json-rpc-websockets"),
                            .product(name: "WebRTC", package: "WebRTC")]),
         .testTarget(
-            name: "BandwidthWebRTCTests",
-            dependencies: ["BandwidthWebRTC"]),
+            name: "BandwidthRTCTests",
+            dependencies: ["BandwidthRTC"]),
     ]
 )
