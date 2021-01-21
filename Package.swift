4ae295d2e32bc10c6bad04ab938ca494f17c10c8
Mike Hamer <mhamer@bandwidth.com>
2021-01-21 16:13:03 -0500

    Grab JSON RPC from the dev branch for now

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Bandwidth/json-rpc-websockets.git", .branch("main")),
+        .package(url: "https://github.com/Bandwidth/json-rpc-websockets.git", .branch("dev")),
         .package(url: "https://github.com/alexpiezo/WebRTC.git", .upToNextMajor(from: "1.1.31567"))
     ],
     targets: [
